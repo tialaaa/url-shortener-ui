01d1b4d10695cc2c0c0d7739e7311fd11d0d5ea9
Tiala Young <tialayoung@gmail.com>
2023-06-12 09:43:01 -0600

    Write postUrls function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,19 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrls = (formInput) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    body: JSON.stringify(formInput),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => {
+    response.json();
+    console.log(response)
+  })
+}
+
+// {long_url: <String>, title: <String>}
